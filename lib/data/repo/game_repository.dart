4f139ce8b9703616b1601bc2e33a52582865dc8e
Ferico5 <huangferico@gmail.com>
2025-01-10 19:41:59 +0700

    First Commit

---
+++ lib/data/repo/game_repository.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:latihan_api_bloc/core/api_client.dart';
+import 'package:latihan_api_bloc/data/models/game_models.dart';
+
+class GameRepository extends ApiClient{
+  Future<List<GameModel>> fetchGames()async{
+    try{
+      var response = await dio.get('game/');
+      debugPrint('GET ALL games : ${response.data}');
+      List list = response.data;
+      List<GameModel> listGameModel = list.map((element)=>GameModel.fromJson(element)).toList();
+      return listGameModel;
+    }on DioException catch (e) {
+      throw Exception(e);
+    }
+  }
+}
