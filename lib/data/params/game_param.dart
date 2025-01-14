f1b01f2e14603e52ec3c107ac8eca0b7d269d159
Ferico5 <huangferico@gmail.com>
2025-01-14 18:12:37 +0700

    tes

--- lib/data/params/game_param.dart
+++ lib/data/params/game_param.dart
@@ -1,3 +1,15 @@
 class GameParam {
+  final int? id;
+  final String name;
+  final String price;
+  final String? status;
 
+  GameParam({required this.id, required this.name, required this.price, required this.status});
+  Map<String, dynamic> toJson() {
+    return {
+      'name': this.name,
+      'price': this.price,
+      'status': this.status
+    };
+  }
 }
