05575235f1a9411814cd867b34e0475917d1f379
Ferico5 <huangferico@gmail.com>
2025-01-14 18:39:10 +0700

    dh lh gk tw bg

--- lib/data/repo/game_repository.dart
+++ lib/data/repo/game_repository.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:latihan_api_bloc/core/api_client.dart';
 import 'package:latihan_api_bloc/data/models/game_models.dart';
+import 'package:latihan_api_bloc/data/params/game_param.dart';
+import 'package:latihan_api_bloc/data/response/game_response.dart';
 
 class GameRepository extends ApiClient{
   Future<List<GameModel>> fetchGames()async{
@@ -12,6 +14,16 @@ class GameRepository extends ApiClient{
       List<GameModel> listGameModel = list.map((element)=>GameModel.fromJson(element)).toList();
       return listGameModel;
     }on DioException catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<GameCreateResponse> createGame(GameParam gameParam) async {
+    try {
+      var response = await dio.post('games');
+      debugPrint('POST Response : ${response.data}');
+      return GameCreateResponse.fromJson(response.data);
+    } on DioException catch(e) {
       throw Exception(e);
     }
   }
---
+++ lib/ui/game/bloc/create/game_create_state.dart
@@ -0,0 +1,19 @@
+part of 'game_create_bloc.dart';
+
+@immutable
+sealed class GameCreateState {}
+
+final class GameCreateInitial extends GameCreateState {}
+
+final class GameCreateLoading extends GameCreateState {}
+
+final class GameCreateLoaded extends GameCreateState {
+  final GameCreateResponse gameCreateResponse;
+
+  GameCreateSuccess({required this.gameCreateResponse});
+}
+
+final class GameCreateError extends GameCreateState {
+  final String message;
+  GameCreateError({required this.message});
+}
